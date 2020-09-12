13dc53ed7c1536329e0a70a8e4d4677eed23c28f
Austin <austinknauer@alumni.colostate.edu>
2020-09-11 23:35:28 -0600

    Updates

---
+++ precache-manifest.c51c73ce724fe105cdea585b7588b22f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f17c8569ac7300b22262c1286fe6cc",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "09204ea0760ac03adbab",
+    "url": "/calculator/static/css/main.6d7571bc.chunk.css"
+  },
+  {
+    "revision": "6980311541708dc0128f",
+    "url": "/calculator/static/js/2.a03eecd8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculator/static/js/2.a03eecd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09204ea0760ac03adbab",
+    "url": "/calculator/static/js/main.4b9fdf5a.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
